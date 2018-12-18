20e54d2401c3eecbe3dde706ebfd1bc3f860ae56
kathishivaramreddy <kathishivaram@gmail.com>
2018-12-19 01:01:48 +0530

    Adding productedit components which popsup when click on product

--- src/ProductList.js
+++ src/ProductList.js
@@ -4,18 +4,39 @@ import './ProductList.css'
 
 export class ProductList extends React.Component{
 
-    render(){
+    constructor(props) {
+        super(props)
+        this.state = {
+            showDetails: false,
+            productId:1
+        }
+        this.openProductBox = this.openProductBox.bind(this)
+        this.closeProductBox = this.closeProductBox.bind(this)
+    }
 
-        const productList = allProducts().products.map( image =>
-            <div className="column">
-            <img className="product" src={require(`./images/${image}`)} alt="productimage"/>
+    openProductBox(id){
+        this.setState({showDetails:true,productId:id})
+    }
+
+    closeProductBox(){
+        this.setState({showDetails:false})
+    }
+
+    render(){
+        console.log(this.state.showDetails,this.state.productId)
+        const productList = allProducts().products.map( product =>
+            <div className="column" onClick={() => this.openProductBox(product.productid)}>
+            <img className="product" src={require(`./images/${product.image}`)} alt="productimage"/>
             </div>)
 
+        const productDetails= allProducts().products.filter(product => product.productid === this.state.productId)
+            .map( product => <div className="productdetails">
+                <img className="product" src={require(`./images/${product.image}`)} alt="productimage"/>
+            </div>)
         return(
-            <div className="row">
-
+            <div className="productDetails">
                 {productList}
-
+                {this.state.showDetails && productDetails}
             </div>
         )
     }
--- src/allProducts.js
+++ src/allProducts.js
@@ -1,6 +1,11 @@
 const allProducts = () => {
     return {
-        products : ["image1.jpeg","image1.jpeg","image1.jpeg","image1.jpeg","image1.jpeg","image1.jpeg"]
+        products : [{productid:1,image:"1.jpeg"},
+                    {productid:2,image:"1.jpeg"},
+                    {productid:3,image:"1.jpeg"},
+                    {productid:4,image:"1.jpeg"},
+                    {productid:5,image:"1.jpeg"},
+                    {productid:6,image:"1.jpeg"}]
     }
 }
 
