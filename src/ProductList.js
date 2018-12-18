f3c87becd374f7f490a62e22aa93f753ac05b057
kathishivaramreddy <kathishivaram@gmail.com>
2018-12-18 23:09:47 +0530

    Separating image data into separate file and reading image from this file

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,36 +1,20 @@
 import React from 'react';
+import allProducts from './allProducts'
 import './ProductList.css'
 
 export class ProductList extends React.Component{
 
     render(){
 
+        const productList = allProducts().products.map( image =>
+            <div className="column">
+            <img className="product" src={require(`./images/${image}`)} alt="productimage"/>
+            </div>)
+
         return(
             <div className="row">
 
-                <div className="column">
-                    <img className="product" src={require('./images/image1.jpeg')} alt="productimage"/>
-                </div>
-
-                <div className="column">
-                    <img className="product" src={require('./images/image1.jpeg')} alt="productimage"/>
-                </div>
-
-                <div className="column">
-                    <img className="product" src={require('./images/image1.jpeg')} alt="productimage"/>
-                </div>
-
-                <div className="column">
-                    <img className="product" src={require('./images/image1.jpeg')} alt="productimage"/>
-                </div>
-
-                <div className="column">
-                    <img className="product" src={require('./images/image1.jpeg')} alt="productimage"/>
-                </div>
-
-                <div className="column">
-                    <img className="product" src={require('./images/image1.jpeg')} alt="productimage"/>
-                </div>
+                {productList}
 
             </div>
         )
---
+++ src/allProducts.js
@@ -0,0 +1,7 @@
+const allProducts = () => {
+    return {
+        products : ["image1.jpeg","image1.jpeg","image1.jpeg","image1.jpeg","image1.jpeg","image1.jpeg"]
+    }
+}
+
+export default allProducts
